ed838d33a6cd5e2ceac1ac65cdfd07d4166f88da
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-05-19 21:03:50 +0300

    add h2 for click

--- js/tabs.js
+++ js/tabs.js
@@ -2,10 +2,21 @@
 
 const tabsHandlerElems = document.querySelectorAll('[data-tabs-handler]');
 const tabsFieldElems = document.querySelectorAll('[data-tabs-field]');
+const sectionTitle = document.querySelectorAll('.section__title');
 
 for (const tab of tabsHandlerElems) {
 
   tab.addEventListener('click', () => {
+
+    sectionTitle.forEach(item => {
+
+      if (item.classList.contains('hidden')) {
+        item.classList.remove('hidden');
+      } else {
+        item.classList.add('hidden');
+      }
+
+    })
 
     tabsHandlerElems.forEach(item => {
       if (tab === item) {
