fe1604b1eee922f206287ecf164bec0c5dc8a2ba
aliaksandr-bandziuk <aliaksandr.bandziuk@gmail.com>
2021-05-20 23:08:38 +0300

    burger and modal

---
+++ js/burgerMenu.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const menuElem = document.querySelector('.menu');
+const humburgerElem = document.querySelector('.humburger-menu');
+
+const toggleMenu = () => {
+
+  menuElem.classList.toggle('menu-active');
+  humburgerElem.classList.toggle('humburger-menu-active');
+
+}
+
+humburgerElem.addEventListener('click', toggleMenu);
